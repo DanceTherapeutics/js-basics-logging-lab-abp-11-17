9f564c6e9a9f9356bc0e2eb068f7baca0255e700
Chris Junker <dancetherapeutics@gmail.com>
2017-11-17 17:58:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const driver = 'bob';
-function expect(spy.calledWithExactly('This code was called')){
+function expect(spy.calledWithExactly('this code was called')){
   return console.log('This code was called');
 }
